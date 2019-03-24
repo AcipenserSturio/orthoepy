dbaaf294902683a9e17f85f692246f5f88b0b847
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2019-03-24 15:01:35 +0500

    Div autogeneration

--- scripts.js
+++ scripts.js
@@ -13,6 +13,8 @@ class Choice {
 }
 function main() {
 	generateChoices();
+	generateDisplays();
+	updateAllStats();
 	currentChoice = 0;
 	displayChoice();
 }
@@ -42,26 +44,24 @@ function updateStats(selectedChoice) {
 		var rightWidth = 50;
 		var leftWidth = 50;
 	} else {
-		var rightWidth = Math.round(100 * choices[selectedChoice].rightanswers / (choices[selectedChoice].rightanswers + choices[selectedChoice].wronganswers))
+		var rightWidth = 100 * choices[selectedChoice].rightanswers / (choices[selectedChoice].rightanswers + choices[selectedChoice].wronganswers);
 		var leftWidth = 100 - rightWidth;
 	}
-	
-	document.getElementById("choice" + selectedChoice + "stats").innerHTML = 
-	"<div class=\"choicename\" id=\"choice" + selectedChoice + "name\">"
-	+ choices[selectedChoice].ambiguous 
-	+ "</div>" 
-	+ "<div style=\"width: "+ rightWidth + "%\" class=\"choiceright\" id=\"choice" + selectedChoice + "right\">" 
-	+ choices[selectedChoice].rightanswers 
-	+ "</div>" 
-	+ "<div style=\"width: "+ leftWidth + "%\" class=\"choicewrong\" id=\"choice" + selectedChoice + "wrong\">" 
-	+ choices[selectedChoice].wronganswers 
-	+ "</div></div>";
+	document.getElementById("choice" + selectedChoice + "name").innerHTML = choices[selectedChoice].ambiguous;
+	document.getElementById("choice" + selectedChoice + "right").innerHTML = choices[selectedChoice].rightanswers;
+	document.getElementById("choice" + selectedChoice + "wrong").innerHTML = choices[selectedChoice].wronganswers;
+	document.getElementById("choice" + selectedChoice + "right").style.width = rightWidth + "%";
+	document.getElementById("choice" + selectedChoice + "wrong").style.width = leftWidth + "%";
 }
 function generateChoices() {
 	choices[0] = new Choice("щаве́ль", "ща́вель", "щавель");
 	choices[1] = new Choice("звони́т", "зво́нит", "звонит");
-	updateStats(0);
-	updateStats(1);
+	choices[2] = new Choice("апостро́ф", "апо́строф", "апостроф");
+}
+function updateAllStats() {
+	for (var i = 0; i < choices.length; i++) {
+		updateStats(i);
+	}
 }
 function nextQuestion() {
 	currentChoice = pickNewChoice();
@@ -72,5 +72,29 @@ function pickNewChoice() {
 		return currentChoice + 1;
 	} else {
 		return 0;
+	}
+}
+function generateDisplays() {
+	for (var i = 0; i < choices.length; i++) {
+		var choiceStats = document.createElement('div');
+		choiceStats.className = "choicestats"
+		choiceStats.id = "choice" + i + "stats";
+		document.getElementById("stats").appendChild(choiceStats);
+		
+		var choiceName = document.createElement('div');
+		var choiceRight = document.createElement('div');
+		var choiceWrong = document.createElement('div');
+		choiceName.className = "choicename"
+		choiceRight.className = "choiceright"
+		choiceWrong.className = "choicewrong"
+		choiceName.id = "choice" + i + "name";
+		choiceRight.id = "choice" + i + "right";
+		choiceWrong.id = "choice" + i + "wrong";
+		document.getElementById("choice" + i + "stats").appendChild(choiceName);
+		document.getElementById("choice" + i + "stats").appendChild(choiceRight);
+		document.getElementById("choice" + i + "stats").appendChild(choiceWrong);
+		document.getElementById("choice" + i + "name").innerHTML = choices[i].ambiguous;
+		document.getElementById("choice" + i + "right").innerHTML = choices[i].rightanswers;
+		document.getElementById("choice" + i + "wrong").innerHTML = choices[i].wronganswers;
 	}
 }
