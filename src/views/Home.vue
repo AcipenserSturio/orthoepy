26bc23e98c0645ac830f5179d1ca66af042b34b2
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-07-04 21:33:56 +0500

    Move previous homepage code to the 'Home' view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div align="center">
+    <h1>ТРЕНАЖЁР ПРАВИЛ РУССКОГО ЯЗЫКА</h1>
+    <div class="text">
+      <p>Добро пожаловать в тренажёр для закрепления правил русского языка.</p>
+      <p>Выберите тему, которую вы будете закреплять:</p>
+    </div>
+    <div class="button"><a href="orthoepy">Орфоэпия</a></div>
+    <div class="button"><a href="nn">Н/НН</a></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
   components: {
-    HelloWorld,
   },
 };
 </script>
