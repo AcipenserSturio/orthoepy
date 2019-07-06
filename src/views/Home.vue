fb1bc5e6276da07d592365f5fc455e92c314ce80
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-07-06 16:17:53 +0500

    Bulmafy 'Home'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,38 @@
 <template>
-  <div align="center">
-    <h1>ТРЕНАЖЁР ПРАВИЛ РУССКОГО ЯЗЫКА</h1>
-    <div class="text">
-      <p>Добро пожаловать в тренажёр для закрепления правил русского языка.</p>
-      <p>Выберите тему, которую вы будете закреплять:</p>
-    </div>
-    <div class="button"><router-link :to="{ name: 'orthoepy' }">Орфоэпия</router-link></div>
-    <div class="button"><router-link :to="{ name: 'single-or-double-n' }">Н/НН</router-link></div>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Тренажер правил русского языка
+          </h1>
+          <h2 class="subtitle">
+            Добро пожаловать в тренажер для закрепления правил русского языка.
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <p class="title is-5">
+          Выберите тему, которую будете закреплять:
+        </p>
+        <div class="list is-hoverable">
+          <router-link :to="{ name: 'orthoepy' }" class="list-item">
+            Орфоэпия
+          </router-link>
+          <router-link :to="{ name: 'single-or-double-n' }" class="list-item">
+            Н/НН
+          </router-link>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
-  components: {
-  },
 };
 </script>
