ee06c928276b7623b3ea5bf853aa5d3b960299ab
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-01 03:18:14 +0500

    Add component for a generic prompt

---
+++ src/components/VPrompt.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <VPromptCheckbox
+      :checkboxes="prompt.checkboxes"
+      v-if="prompt.type === 'checkbox'"
+    />
+    <VPromptRadio
+      :radios="prompt.radios"
+      v-else-if="prompt.type === 'radio'"
+    />
+    <VPromptText
+      :placeholder="prompt.placeholder"
+      v-else-if="prompt.type === 'text'"
+    />
+    <p
+      class="is-italic"
+      v-else
+    >
+      Не удалось отобразить поле ввода ответа.
+    </p>
+  </div>
+</template>
+
+<script>
+import VPromptCheckbox from '@/components/VPromptCheckbox.vue';
+import VPromptRadio from '@/components/VPromptRadio.vue';
+import VPromptText from '@/components/VPromptText.vue';
+import { Prompt } from '@/models';
+
+export default {
+  name: 'VPrompt',
+  components: {
+    VPromptCheckbox,
+    VPromptRadio,
+    VPromptText,
+  },
+  props: {
+    prompt: Prompt,
+  },
+};
+</script>
+
+<style scoped>
+</style>
