e206e288f5f74acae925e09311a5558aa103efd7
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-01 03:18:00 +0500

    Add component for a text prompt

---
+++ src/components/VPromptText.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-field>
+    <b-input
+      v-model="givenAnswer"
+      :placeholder="placeholder">
+    </b-input>
+  </b-field>
+</template>
+
+<script>
+import BInput from 'buefy/src/components/input/Input.vue';
+import BField from 'buefy/src/components/field/Field.vue';
+
+export default {
+  name: 'VPromptText',
+  components: {
+    BInput,
+    BField,
+  },
+  props: {
+    placeholder: String,
+  },
+  data() {
+    return {
+      givenAnswer: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
