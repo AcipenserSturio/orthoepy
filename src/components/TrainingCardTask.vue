78cb261c38287ada7bcb50247c4829c0e42b8813
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-01 03:18:51 +0500

    Add component for a card training's task

---
+++ src/components/TrainingCardTask.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ taskNumber }}/{{ tasksTotal }} — {{ test.topic }}
+      </p>
+    </header>
+
+    <div class="card-content">
+      <div class="content">
+        <p>{{ task.question }}</p>
+        <br>
+        <VPrompt :prompt="task.prompt"/>
+      </div>
+    </div>
+
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Ответить</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+import VPrompt from '@/components/VPrompt.vue';
+import { Test } from '@/models';
+
+export default {
+  name: 'TrainingCardTask',
+  components: {
+    VPrompt,
+  },
+  props: {
+    taskIndex: Number,
+    test: Test,
+  },
+  computed: {
+    task() {
+      return this.test.tasks[this.taskIndex];
+    },
+    taskNumber() {
+      return this.taskIndex + 1;
+    },
+    tasksTotal() {
+      return this.test.tasks.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
