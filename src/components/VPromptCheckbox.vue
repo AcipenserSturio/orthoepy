06f22b9d3042afc6243b5e324b4100f70de9660d
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-01 03:16:53 +0500

    Add component for a checkbox prompt

---
+++ src/components/VPromptCheckbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="block">
+    <div class="field" v-for="checkbox in checkboxes" :key="checkbox">
+      <b-checkbox
+        v-model="givenAnswer"
+        :native-value="checkbox"
+      >
+        {{ checkbox }}
+      </b-checkbox>
+    </div>
+  </div>
+</template>
+
+<script>
+import BCheckbox from 'buefy/src/components/checkbox/Checkbox.vue';
+
+export default {
+  name: 'VPromptCheckbox',
+  components: {
+    BCheckbox,
+  },
+  props: {
+    checkboxes: Array,
+  },
+  data() {
+    return {
+      givenAnswer: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
