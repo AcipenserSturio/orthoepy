1618e76514c8f025b45074445c82485191501480
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-01 03:17:29 +0500

    Add component for a radio prompt

---
+++ src/components/VPromptRadio.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="field" v-for="radio in radios" :key="radio">
+      <b-radio
+        v-model="givenAnswer"
+        :native-value="radio"
+      >
+        {{ radio }}
+      </b-radio>
+    </div>
+  </div>
+</template>
+
+<script>
+import BRadio from 'buefy/src/components/radio/Radio.vue';
+
+export default {
+  name: 'VPromptRadio',
+  components: {
+    BRadio,
+  },
+  props: {
+    radios: Array,
+  },
+  data() {
+    return {
+      givenAnswer: String,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
