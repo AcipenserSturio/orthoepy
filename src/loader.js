f9cc383b3f8036ff8d0acb70534bfce56cc46874
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-02 18:09:41 +0500

    Add test loader and a test case

---
+++ src/loader.js
@@ -0,0 +1,27 @@
+import { Test } from '@/models';
+
+const testPathsForTrainings = {
+  card: {
+    test: 'assets/test.json',
+  },
+};
+
+export async function loadTest(topic, trainingType) {
+  const testPaths = testPathsForTrainings[trainingType];
+  if (!testPaths) {
+    return null;
+  }
+
+  const path = testPaths[topic];
+  if (!path) {
+    return null;
+  }
+
+  let rawTest;
+  try {
+    rawTest = await import(`@/${path}`);
+  } catch (e) {
+    return null;
+  }
+  return new Test(rawTest);
+}
