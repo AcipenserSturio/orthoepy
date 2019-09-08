3591c9f4328cbb7c3b4268615ef5762bf8febc39
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-09 00:34:46 +0500

    Add 'TrainingCardResult' that shows up on test completion

--- src/views/TrainingCardView.vue
+++ src/views/TrainingCardView.vue
@@ -7,22 +7,34 @@
       :class="{ [isCorrect ? 'card-correct' : 'card-incorrect']: isChecking }"
     >
       <header class="card-header">
-        <p class="card-header-title">
+        <p v-if="!isCompleted" class="card-header-title">
           {{ taskNumber }}/{{ tasksTotal }} — {{ test.topic }}
+        </p>
+        <p v-else class="card-header-title">
+          Результаты — {{ test.topic }}
         </p>
       </header>
 
       <div class="card-content">
         <TrainingCardTask
+          v-if="!isCompleted"
           :task="currentTask"
           :is-checking="isChecking"
           :is-correct="isCorrect"
           v-model="currentAnswer"
         />
+        <TrainingCardResult
+          v-else
+          :tasks-correct="tasksCorrect"
+          :tasks-total="tasksTotal"
+        />
       </div>
 
       <footer class="card-footer">
-        <a v-if="!isChecking" class="card-footer-item" @click="onAnswer">
+        <router-link v-if="isCompleted" class="card-footer-item" :to="{ name: 'home' }">
+          На главную
+        </router-link>
+        <a v-else-if="!isChecking" class="card-footer-item" @click="onAnswer">
           Ответить
         </a>
         <a v-else-if="!isLastTask" class="card-footer-item" @click="onContinue">
@@ -48,10 +60,12 @@
 <script>
 import TrainingCardTask from '@/components/TrainingCardTask.vue';
 import { loadTest } from '@/loader';
+import TrainingCardResult from '@/components/TrainingCardResult.vue';
 
 export default {
   name: 'TrainingCardView',
   components: {
+    TrainingCardResult,
     TrainingCardTask,
   },
   data() {
@@ -64,6 +78,7 @@ export default {
 
       isChecking: false,
       isCorrect: null,
+      isCompleted: false,
     };
   },
   computed: {
@@ -72,6 +87,15 @@ export default {
     },
     tasksTotal() {
       return this.test.tasks.length;
+    },
+    tasksCorrect() {
+      let count = 0;
+
+      this.testUserAnswers.forEach((answer) => {
+        if (answer.isCorrect) count += 1;
+      });
+
+      return count;
     },
     currentTask() {
       return this.test.tasks[this.taskIndex];
@@ -99,7 +123,10 @@ export default {
     onAnswer() {
       this.isChecking = true;
       this.isCorrect = this.currentTask.checkAnswer(this.currentAnswer);
-      this.testUserAnswers[this.taskIndex] = this.currentAnswer;
+      this.testUserAnswers[this.taskIndex] = {
+        userAnswer: this.currentAnswer,
+        isCorrect: this.isCorrect,
+      };
     },
     onContinue() {
       this.currentAnswer = null;
@@ -113,7 +140,7 @@ export default {
       this.isChecking = false;
       this.isCorrect = null;
 
-      alert('You are done.');
+      this.isCompleted = true;
     },
   },
 };
