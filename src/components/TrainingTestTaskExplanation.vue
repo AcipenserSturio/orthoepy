d37a8d306142d02480fb5de2ade2c48a1a90e43b
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-10 22:37:07 +0500

    Add result, checking label and explanation analogs for 'test'

---
+++ src/components/TrainingTestTaskExplanation.vue
@@ -0,0 +1,33 @@
+<template>
+  <details>
+    <summary
+      class="accordion-control has-text-weight-semibold"
+      style="margin-bottom: 1rem"
+    >
+      Пояснение
+    </summary>
+    <TaskExplanation :explanation="explanation"/>
+  </details>
+</template>
+
+<script>
+import TaskExplanation from '@/components/TaskExplanation.vue';
+import { Explanation } from '@/models';
+
+export default {
+  name: 'TrainingTestTaskExplanation',
+  components: {
+    TaskExplanation,
+  },
+  props: {
+    explanation: Explanation,
+  },
+};
+</script>
+
+<style scoped>
+.accordion-control {
+  cursor: pointer;
+  outline: none;
+}
+</style>
