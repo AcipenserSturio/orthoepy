8d3e61267bf3671b135b9abee099088ed4fd46f0
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-11 00:49:25 +0500

    Add a button with description for each 'not' training

--- src/views/NotHomeView.vue
+++ src/views/NotHomeView.vue
@@ -12,19 +12,48 @@
 
     <section class="section">
       <div class="container">
-        <div class="has-text-centered">
-          <b-button @click="openTraining">
-            Открыть тренажер
-          </b-button>
-        </div>
-        <hr>
-        <div class="content">
-          <p class="title is-5">
-            Как пользоваться
-          </p>
-          <p>
-            Здесь когда-нибудь появится справка.
-          </p>
+        <div class="columns">
+
+          <div class="column is-one-third">
+            <div class="has-text-centered">
+              <b-button @click="openRuleTraining">
+                Тренировать правила
+              </b-button>
+
+              <p class="is-size-7" style="margin-top: 1rem">
+                Здесь вы можете изучить правила слитного и раздельно
+                правописания частицы "не" в формате <strong>карточек</strong>.
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-third">
+            <div class="has-text-centered">
+              <b-button @click="openAlgorithmTraining">
+                Тренировать алгоритм
+              </b-button>
+
+              <p class="is-size-7" style="margin-top: 1rem">
+                Тренировка алгоритма, по которому можно определить, писать слово
+                слитно или раздельно, в формате <strong>карточек</strong>.
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-third">
+            <div class="has-text-centered">
+              <b-button @click="openTaskTraining">
+                Тренировать задания
+              </b-button>
+
+              <p class="is-size-7" style="margin-top: 1rem">
+                Вам предложат задания в формате <strong>теста</strong>, где
+                необходимо будет указать правильное написание: слитное или
+                раздельное.
+              </p>
+            </div>
+          </div>
+          
         </div>
       </div>
     </section>
@@ -38,8 +67,14 @@ export default {
   name: 'NotHomeView',
   components: { BButton },
   methods: {
-    openTraining() {
-      alert('Doesn\'t work right now.');
+    openRuleTraining() {
+      alert('Doesn\'t work right now (rule).');
+    },
+    openAlgorithmTraining() {
+      alert('Doesn\'t work right now (algorithm).');
+    },
+    openTaskTraining() {
+      alert('Doesn\'t work right now (task).');
     },
   },
 };
