7c38835bf4283ba1f8fe81c97371718b613db58c
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-09-16 23:14:16 +0500

    Add 'not-rules' card test getter

--- src/tester.js
+++ src/tester.js
@@ -31,7 +31,33 @@ export async function getTest(topic, trainingType) {
 
 
 async function getNotRulesTest() {
-  
+  const deadlockRules = (await import('@/assets/not_deadlock_rules')).default;
+
+  const title = 'Слитное и раздельное правописание "не"';
+  let tasks = deadlockRules.map((deadlockRule) => {
+    const pos = deadlockRule.pos;
+    const rule = deadlockRule.rule;
+    const spelling = deadlockRule.spelling;
+
+    let title;
+    if (rule) {
+      title = `${rule}.`;
+    } else if (pos.split(', ').length === 1) {
+      title = `С частью речи: ${pos.toLowerCase()}.`;
+    } else {
+      title = `С частями речи: ${pos.toLowerCase()}.`;
+    }
+
+    return new Task(
+      title,
+      spelling,
+      new RadioPrompt(['Слитно', 'Раздельно']),
+      null,
+    );
+  });
+  shuffle(tasks);
+
+  return new Test(title, tasks);
 }
 
 
