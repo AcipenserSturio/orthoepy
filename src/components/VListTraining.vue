c300996f8dd327423abaa1969ecef1edabdb9bdf
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-15 17:08:56 +0500

    Add 'VListTraining' component

---
+++ src/components/VListTraining.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list is-hoverable">
+    <router-link
+      class="list-item"
+      v-for="(training, i) of trainings"
+      :key="`training-${i}`"
+      :to="{ name: `training-${training.type}`, params: { 'topic': training.topic } }"
+    >
+      {{ training.title }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VListTraining',
+  props: {
+    trainings: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
