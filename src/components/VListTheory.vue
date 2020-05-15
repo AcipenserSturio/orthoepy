e00ddfd6ed5dce09caf3c00fed671e90c3b6f104
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-15 17:09:12 +0500

    Add 'VListTheory' component

---
+++ src/components/VListTheory.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-collapse
+      class="card"
+      v-for="(theory, i) of theories"
+      :key="`theory-${i}`"
+      animation="slide"
+      :open="openTheoryIndex === i"
+      @open="openTheoryIndex = i"
+    >
+      <div
+        class="card-header"
+        role="button"
+        slot="trigger"
+        slot-scope="props"
+      >
+        <p class="card-header-title is-uppercase">
+          {{ theory.title }}
+        </p>
+        <a class="card-header-icon">
+          <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+        </a>
+      </div>
+      <div class="card-content content" v-html="marked(theory.text)">
+      </div>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+import BCollapse from 'buefy/src/components/collapse/Collapse';
+import BIcon from 'buefy/src/components/icon/Icon';
+
+export default {
+  name: 'VListTheory',
+  components: {
+    BCollapse,
+    BIcon,
+  },
+  props: {
+    theories: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      openTheoryIndex: -1,
+    };
+  },
+  methods: {
+    marked(text) {
+      return marked(text);
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
