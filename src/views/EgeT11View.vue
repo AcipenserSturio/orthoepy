f3654a5a9d7d3ef8f730a21114775af735cc4c7d
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-15 17:09:30 +0500

    Integrate 'VListTheory' and 'VListTraining' components

--- src/views/EgeT11View.vue
+++ src/views/EgeT11View.vue
@@ -12,39 +12,7 @@
             <p class="title is-5">
               Теория
             </p>
-            <b-collapse
-              class="card"
-              animation="slide"
-              v-for="(theory, index) of theoryItems"
-              :key="getTheoryKey(index)"
-              :open="isOpen === index"
-              @open="isOpen = index">
-              <div
-                slot="trigger"
-                slot-scope="props"
-                class="card-header"
-                role="button">
-                <p class="card-header-title">
-                  {{ theory.title }}
-                </p>
-                <a class="card-header-icon">
-                  <b-icon
-                    :icon="props.open ? 'menu-down' : 'menu-up'">
-                  </b-icon>
-                </a>
-              </div>
-              <div class="card-content">
-                <div
-                  v-for="(subtheory, subindex) of theory.subitems"
-                  :key="getSubtheoryKey(subindex)">
-                  <hr v-if="subindex !== 0">
-                  <p class="title is-6">
-                    {{ subtheory.title }}
-                  </p>
-                  <div class="content" v-html="marked(subtheory.text)"></div>
-                </div>
-              </div>
-            </b-collapse>
+            <v-list-theory :theories="theories"/>
           </div>
         </section>
       </div>
@@ -54,23 +22,7 @@
             <p class="title is-5">
               Списки слов для закрепления
             </p>
-            <div class="list is-hoverable">
-              <router-link :to="{ name: 'training-card', params: { 'topic': 'ege-t11-suffices-ik-ek' } }" class="list-item">
-                Существительные. Суффиксы ИК/ЕК
-              </router-link>
-              <router-link :to="{ name: 'training-card', params: { 'topic': 'ege-t11-suffices-ic-ec' } }" class="list-item">
-                Существительные. Суффиксы ИЦ/ЕЦ
-              </router-link>
-              <router-link :to="{ name: 'training-card', params: { 'topic': 'ege-t11-suffices-ichk-echk' } }" class="list-item">
-                Существительные. Суффиксы ИЧК/ЕЧК
-              </router-link>
-              <router-link :to="{ name: 'training-card', params: { 'topic': 'ege-t11-suffices-ink-enk' } }" class="list-item">
-                Существительные. Суффиксы ИНК/ЕНК
-              </router-link>
-              <router-link :to="{ name: 'training-card', params: { 'topic': 'ege-t11-nouns-mya' } }" class="list-item">
-                Существительные. Слова на -МЯ
-              </router-link>
-            </div>
+            <v-list-training :trainings="trainings"/>
           </div>
         </section>
       </div>
@@ -84,56 +36,51 @@ import BCollapse from 'buefy/src/components/collapse/Collapse';
 import BIcon from 'buefy/src/components/icon/Icon';
 import BButton from 'buefy/src/components/button/Button';
 import VNavbar from '../components/VNavbar';
+import VListTraining from '../components/VListTraining';
+import VListTheory from '../components/VListTheory';
 
 export default {
   name: "EgeT11View",
-  components: { VNavbar, BCollapse, BIcon, BButton },
+  components: { VListTheory, VListTraining, VNavbar, BCollapse, BIcon, BButton },
   data() {
     return {
-      isOpen: -1,
-      theoryItems: [
+      theories: [
         {
           title: 'Существительные',
-          subitems: [
-            {
-              title: 'Суффикс -ЕК или -ИК (-НИК, -ЧИК)?',
-              text: 'Если поставить слово в родительный падеж, то гласная из суффикса может "выпасть". Если гласная "выпадает", то -ЕК, иначе -ИК. Примеры: карандашик - нет карандашика; человечек - нет человечка.',
-            },
-            {
-              title: 'Суффикс -ЕЦ или -ИЦ?',
-              text: 'Зависит от рода имени существительного. Для мужского рода: -ЕЦ. Для женского рода: -ИЦ. Для среднего рода: -ЕЦ, если ударение падает на слог после суффикса (может пропадать в именительном падеже, например: блюдцо - блюдец), и -ИЦ, если ударение предшедствует суффиксу.',
-            },
-            {
-              title: 'Суффикс -ИЧК или -ЕЧК?',
-              text: '-ИЧК пишется в словах женского рода, образованных от слов с суффиксом -ИЦ. В остальных случаях пишется -ЕЧК (например, если образовно от слов на -МЯ, слов с суффиксом -ЕК или -К или от сокращенных мужских имен на -ИК, таких как Эрик, Алик).',
-            },
-            {
-              title: 'Суффикс -ИНК или -ЕНК?',
-              text: '-ИНК пишется в существительных образованных от слов женского рода на -ИН-А, например: соломинка. Исключение: горлинка. -ЕНК пишется в остальных случаях (например, в существительных женского рода, обозначающих лиц женского пола, например: француженка, монашенка).\n\n*Исключение: горлинка*',
-            },
-            {
-              title: 'Правописание существительных, оканчивающихся на -МЯ.',
-              text: 'В разносклоняемых именах существительных, оканчивающихся на -МЯ (например, время или имя) при склонении и образовании новых однокоренных слов пишется суффикс -ЕН(ЕНН)- (например, время - времени, бремя - обременить). Но если слово на -МЯ является первой частью сложного слова, то оно находится там в своей начальной форме (с гласной Я) (например, времяпрепровождение).',
-            },
-          ],
+          text: '**Суффикс -ЕК или -ИК (-НИК, -ЧИК)?**\n\n> Если поставить слово в родительный падеж, то гласная из суффикса может\n> "выпасть". Если гласная "выпадает", то -ЕК, иначе -ИК. Примеры: карандашик -\n> нет карандашика; человечек - нет человечка. \n\n---\n\n**Суффикс -ЕЦ или -ИЦ?**\n\n> Зависит от рода имени существительного. Для мужского рода: -ЕЦ. Для женского\n> рода: -ИЦ. Для среднего рода: -ЕЦ, если ударение падает на слог после суффикса\n> (может пропадать в именительном падеже, например: блюдцо - блюдец), и -ИЦ,\n> если ударение предшедствует суффиксу.\n\n---\n\n**Суффикс -ИЧК или -ЕЧК?**\n\n> -ИЧК пишется в словах женского рода, образованных от слов с суффиксом -ИЦ. В\n> остальных случаях пишется -ЕЧК (например, если образовно от слов на -МЯ,\n> слов с суффиксом -ЕК или -К или от сокращенных мужских имен на -ИК, таких как\n> Эрик, Алик). В русском языке нет безударного суффикса -ЯЧК.\n\n---\n\n**Суффикс -ИНК или -ЕНК?**\n\n> -ИНК пишется в существительных образованных от слов женского рода на -ИН-А,\n> например: соломинка. Исключение: горлинка. -ЕНК пишется в остальных случаях\n> (например, в существительных женского рода, обозначающих лиц женского пола,\n> например: француженка, монашенка).\n>\n> **Исключение:** горлинка\n\n---\n\n**Правописание существительных, оканчивающихся на -МЯ.**\n\n> В разносклоняемых именах существительных, оканчивающихся на -МЯ (например,\n> время или имя) при склонении и образовании новых однокоренных слов пишется\n> суффикс -ЕН(ЕНН)- (например, время - времени, бремя - обременить). Но если\n> слово на -МЯ является первой частью сложного слова, то оно находится там в\n> своей начальной форме (с гласной Я) (например, времяпрепровождение).\n\n---\n\n**Постоянные суффиксы существительных.**\n\n> Постоянные суффиксы всегда пишутся одинаково. Некоторые постоянные суффиксы\n> существительных: оньк, еньк, тель, кт, от, изн, ин, ств, онк, ени.',
+        }
+      ],
+      trainings: [
+        {
+          title: 'Существительные. Суффиксы ИК/ЕК',
+          topic: 'ege-t11-suffices-ik-ek',
+          type: 'card',
         },
-      ]
-    }
+        {
+          title: 'Существительные. Суффиксы ИЦ/ЕЦ',
+          topic: 'ege-t11-suffices-ic-ec',
+          type: 'card',
+        },
+        {
+          title: 'Существительные. Суффиксы ИЧК/ЕЧК',
+          topic: 'ege-t11-suffices-ichk-echk',
+          type: 'card',
+        },
+        {
+          title: 'Существительные. Суффиксы ИНК/ЕНК',
+          topic: 'ege-t11-suffices-ink-enk',
+          type: 'card',
+        },
+        {
+          title: 'Существительные. Слова на -МЯ',
+          topic: 'ege-t11-nouns-mya',
+          type: 'card',
+        },
+      ],
+    };
   },
-  methods: {
-    marked(text) {
-      return marked(text);
-    },
-    getTheoryKey(index) {
-      return `theory_${index}`;
-    },
-    getSubtheoryKey(index) {
-      return `subtheory_${index}`;
-    }
-  }
 }
 </script>
 
 <style scoped>
-
 </style>
