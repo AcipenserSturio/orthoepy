aa116bb2276e79be1cc89e3e8ad6563da0625cef
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-18 16:45:47 +0500

    Suppress eslint's error when using template strings in imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ['error', 2, { 'ignoredNodes': ['TemplateLiteral'] }],
+    'template-curly-spacing': ['off'],
   },
   parserOptions: {
     parser: 'babel-eslint',
