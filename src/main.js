6b0d4883ab2bcc782e1dc7c2c6d0135c4e779d68
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-19 18:58:20 +0500

    Install correct Font Awesome dependencies

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Buefy from 'buefy';
 import Vue from 'vue';
 import DOMPurify from 'dompurify';
 import marked from 'marked';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add(fas);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+Vue.use(Buefy, { defaultIconPack: 'fas' });
 
 marked.setOptions({
   sanitizer: DOMPurify.sanitize,
@@ -12,9 +19,6 @@ marked.setOptions({
 });
 
 Vue.config.productionTip = false;
-Vue.use(Buefy, {
-  defaultIconPack: 'fas',
-});
 
 new Vue({
   router,
