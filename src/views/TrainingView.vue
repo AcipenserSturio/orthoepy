9298290d633aab7d050c6c9f8250b45e2fe67ae2
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-09 00:23:57 +0500

    Add training setting, training 404 message; display training's title

--- src/views/TrainingView.vue
+++ src/views/TrainingView.vue
@@ -4,13 +4,14 @@
       title="Тренажер"
       has-home-button
     />
-    <section class="section">
+
+    <section class="section" v-if="training" :key="'training'">
       <div class="columns">
         <div class="column is-half container">
           <div class="card">
             <div class="card-header">
               <div class="card-header-title">
-                Dummy Title
+                {{ training.title }}
               </div>
             </div>
             <div class="card-content">
@@ -27,16 +28,45 @@
         </div>
       </div>
     </section>
+
+    <section class="section" v-else :key="'training-404'">
+      <div class="columns">
+        <div class="column is-half container">
+          <h1 class="title">
+            Увы,
+          </h1>
+          <h2 class="subtitle">
+            но такой тренировки не существует
+          </h2>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import VNavbar from '@/components/VNavbar.vue';
+import getTraining from '@/trainer';
 
 export default {
   name: 'TrainingView',
   components: {
     VNavbar,
+  },
+  beforeRouteEnter(to, from, next) {
+    getTraining(to.params.topic).then((training) => {
+      next(vm => training && vm.setTraining(training));
+    });
+  },
+  data() {
+    return {
+      training: null,
+    };
+  },
+  methods: {
+    setTraining(training) {
+      this.training = training;
+    },
   },
 };
 </script>
