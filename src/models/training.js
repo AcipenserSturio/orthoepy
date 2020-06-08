11ed9c950aedecd00255195506677c3bc8aa58a1
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-08 22:49:41 +0500

    Add 'Training' model

---
+++ src/models/training.js
@@ -0,0 +1,39 @@
+export default class Training {
+  constructor(
+    title,
+    tasks,
+    {
+      hideProgress = false,
+      offerRepeat = false,
+      showMistakesSummary = false,
+    },
+  ) {
+    this.title = title;
+    this.tasks = tasks;
+
+    this.config = {
+      hideProgress,
+      offerRepeat,
+      showMistakesSummary,
+    };
+
+    this.userAnswers = new Array(tasks.length).fill(null);
+    this.userAnswersIsCorrect = new Array(tasks.length).fill(null);
+    this.userAnswersTotalCount = 0;
+    this.userAnswersCorrectCount = 0;
+  }
+
+  userAnswered(taskIndex, userAnswer) {
+    const task = this.tasks[taskIndex];
+
+    const isCorrect = task.prompt.constructor.areEqualByContent(
+      userAnswer,
+      task.correctAnswer,
+    );
+
+    this.userAnswers[taskIndex] = userAnswer;
+    this.userAnswersIsCorrect[taskIndex] = isCorrect;
+    this.userAnswersTotalCount += 1;
+    this.userAnswersCorrectCount += isCorrect ? 1 : 0;
+  }
+}
