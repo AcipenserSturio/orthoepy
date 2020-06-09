0c202aa8a3298c05709ab39cae76f55531dbff2c
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-10 01:20:17 +0500

    Add dummy 'TrainingCardContent' component

---
+++ src/components/TrainingCardContent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card-content">
+    Dummy Text
+  </div>
+</template>
+
+<script>
+import Training from '@/models/training';
+
+export default {
+  name: 'TrainingCardContent',
+  props: {
+    value: {
+      type: undefined,
+      required: true,
+    },
+    training: {
+      type: Object,
+      required: true,
+    },
+    activeTaskIndex: {
+      type: Number,
+      required: true,
+    },
+    activeState: {
+      type: undefined,
+      required: true,
+    },
+  },
+  computed: {
+    isAnsweringState() {
+      return this.activeState === Training.STATE_ANSWERING;
+    },
+    isCheckingState() {
+      return this.activeState === Training.STATE_CHECKING;
+    },
+    isFinishedState() {
+      return this.activeState === Training.STATE_FINISHED;
+    },
+  },
+  methods: {
+    updateValue(newValue) {
+      this.$emit('input', newValue);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
