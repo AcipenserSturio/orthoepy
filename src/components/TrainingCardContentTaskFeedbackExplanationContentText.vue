864824c60f496f888464a5d95f7883cec93ad080
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-11 03:14:43 +0500

    Add 'TrainingCardContentTaskFeedbackExplanationContentText' component

---
+++ src/components/TrainingCardContentTaskFeedbackExplanationContentText.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content" v-html="marked(this.explanation.text)"></div>
+</template>
+
+<script>
+import { TextExplanation } from '@/models/explanations';
+import marked from 'marked';
+
+export default {
+  name: 'TrainingCardContentTaskFeedbackExplanationContentText',
+  props: {
+    explanation: {
+      type: TextExplanation,
+      required: true,
+    },
+  },
+  methods: {
+    marked(text) {
+      return marked(text);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
