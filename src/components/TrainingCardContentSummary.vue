524456caf7cc38c0eed5881226dc2b65af8e6315
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-11 02:39:53 +0500

    Add 'TrainingCardContentSummary' component

---
+++ src/components/TrainingCardContentSummary.vue
@@ -0,0 +1,24 @@
+<template>
+  <TrainingCardContentSummaryLabel
+    :training="training"
+  />
+</template>
+
+<script>
+import Training from '@/models/training';
+import TrainingCardContentSummaryLabel from '@/components/TrainingCardContentSummaryLabel.vue';
+
+export default {
+  name: 'TrainingCardContentSummary',
+  components: { TrainingCardContentSummaryLabel },
+  props: {
+    training: {
+      type: Training,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
