dd3bc1868ec9686ad25190eab3856adb161b9c0e
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-06-11 14:51:31 +0500

    Remove field type's icon from text prompt

--- src/components/TrainingCardContentTaskPromptText.vue
+++ src/components/TrainingCardContentTaskPromptText.vue
@@ -1,13 +1,18 @@
 <template>
   <b-field :type="type">
-    <b-input
-      ref="input"
-      autofocus
-      :disabled="disabled"
-      :placeholder="prompt.placeholder"
-      :value="value"
-      @input="updateValue"
-    ></b-input>
+    <div class="control">
+      <input
+        ref="input"
+        autofocus
+        class="input"
+        :class="type"
+        type="text"
+        :disabled="disabled"
+        :placeholder="prompt.placeholder"
+        :value="value"
+        @input="onInput"
+      >
+    </div>
   </b-field>
 </template>
 
@@ -37,15 +42,27 @@ export default {
     },
   },
   watch: {
-    disabled(oldValue, newValue) {
-      if (newValue === true) {
-        this.$refs.input.focus();
+    disabled(newDisabled) {
+      if (newDisabled === false) {
+        this.$nextTick(() => this.$refs.input.focus());
       }
     },
   },
   methods: {
     updateValue(newValue) {
       this.$emit('input', newValue);
+    },
+    onInput(event) {
+      // Source: Buefy's BInput
+      //
+      // Input's 'input' event listener, 'nextTick' is used to prevent event
+      // firing before ui update, helps when using masks (Cleavejs and
+      // potentially others).
+      this.$nextTick(() => {
+        if (event.target) {
+          this.updateValue(event.target.value);
+        }
+      });
     },
   },
 };
